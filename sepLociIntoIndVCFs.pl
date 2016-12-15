a231f8b9b08a99ab4e4885ce6fe62af97b194d58
atcg <evanmelstad@ucla.edu>
2016-12-15 13:19:15 -0800

    Separating loci into their own VCF files

---
+++ sepLociIntoIndVCFs.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Parallel::ForkManager;
+
+# The file "loci" contains the locus ("chromosome") names from the VCF. Harvest these using:
+#    grep contig= ../HSEM031-040.bqsr.snps.q30.passOnly.minGQ20MaxMiss90p.recode.vcf | perl -p -e 's/^\#\#contig\=\<ID\=//g' | perl -p -e 's/\,.*//g' > loci
+open(my $locusFH, "<", "loci") or die "couldn't open loci for reading: $!\n";
+
+# Run 30 threads simulataneously
+my $fm = Parallel::ForkManager->new(30);
+
+while (my $locus = <$locusFH>) {
+    $fm->start and next;
+    chomp($locus);
+
+    my $locusVCF = "\'" . $locus . ".vcf\'";
+    $locusVCF =~ s/\|/\-\-/g;
+
+    
+    # This GATK command pulls out the variants that match "locus"
+    system("java -Xmx3g -jar /home/evan/bin/GATK/GenomeAnalysisTK.jar -R ../../reference/all.RBBH.fasta -T SelectVariants -V /mnt/Data1/CTScombined/haplotyping/HSEM031-040.bqsr.snps.q30.passOnly.minGQ20MaxMiss50p.nuke3sparse.beaglePhased.rename.vcf -L \'$locus\' -o $locusVCF");
+
+    $fm->finish;
+}
+$fm->wait_all_children();
